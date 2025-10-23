301fee344088906ca04a33755d1a14057368b53e
kimdaechul <rlaeocjf@gmail.com>
2025-10-23 15:26:45 +0900

    java study

--- Java20251023/src/ex01/If조건문06.java
+++ Java20251023/src/ex01/If조건문06.java
@@ -16,7 +16,25 @@ public class If조건문06 {
 		 * 나이 xx는 xx요금 입니다.
 		 */
 		
+		int age  = 4;    //나이
+		int fee = 2000;   //요금
+		double rate = 0;  //할인율
 		
+		if(age>65) {
+			fee = 0;
+		}else if(age>=20) {
+			rate = 0;
+		}else if(age>=15) {
+			rate = 0.2;
+		}else if(age>=7) {
+			rate = 0.5;
+		}else {
+			fee = 0;
+		}
+		
+		fee = (int)(fee * (1 - rate));
+		
+		System.out.println("나이 " + age + "는 " + fee+"요금 입니다.");
 				
 		System.out.println("------프로그램 종료--------");
 

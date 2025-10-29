cbf23f6692ca82563d0039c3ff9b085941341ba1
kimdaechul <rlaeocjf@gmail.com>
2025-10-29 13:12:32 +0900

    java study

---
+++ Java20251028_클래스/src/ex08/MemberService.java
@@ -0,0 +1,20 @@
+package ex08;
+
+public class MemberService {
+
+	private String id;
+	private String password;
+	
+	public boolean login(String id, String password) {
+		
+		if(id.equals("hong") && password.equals("12345")) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void logout(String id) {
+		System.out.println(id + "님이 로그아웃 되었습니다.");
+	}
+}

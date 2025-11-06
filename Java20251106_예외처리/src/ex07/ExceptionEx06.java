c96b19d25468a96aefb2d23ffe9204ff596a52ea
kimdaechul <rlaeocjf@gmail.com>
2025-11-06 11:49:01 +0900

    java study

--- Java20251106_예외처리/src/ex07/ExceptionEx06.java
+++ Java20251106_예외처리/src/ex07/ExceptionEx06.java
@@ -2,10 +2,9 @@ package ex07;
 
 public class ExceptionEx06 {
 
-	public static void main(String[] args) throws  ArithmeticException{
+	public static void main(String[] args) throws  Exception{
 
 		System.out.println("프로그램 시작...");
-		
 		
 		func1(5, 0);
 		
@@ -13,13 +12,13 @@ public class ExceptionEx06 {
 		System.out.println("프로그램 종료...");
 	}
 
-	private static void func1(int i, int j) throws ArithmeticException{
+	private static void func1(int i, int j) throws Exception{
 		
 		func2(i,j);
 		
 	}
 
-	private static void func2(int i, int j) throws ArithmeticException{
+	private static void func2(int i, int j) throws Exception{
 		
 		int result = i/j;
 		System.out.println(result);
